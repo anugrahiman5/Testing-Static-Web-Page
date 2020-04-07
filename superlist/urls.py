1bb10816fd6197de029d03c9ced434ebf2deaf10
anugrahiman5 <anugrah.iman.5>
2020-04-07 19:45:36 +0700

    Testing a Static Web

---
+++ superlist/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.contrib import admin
+from lists import views
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("<title>localhost</title><h1>Anugrah Iman Setyo Utomo</h1>")
+
+urlpatterns = [
+	#url(r'^$', 'home', name='home'),
+	#url(r'^$', views.home_page, name='home'),
+	url(r'^admin/', admin.site.urls),
+	url(r'^$', index),
+]
